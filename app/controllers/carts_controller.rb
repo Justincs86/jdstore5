6c0cab30792f0f5c25befb73f312f6d002866cc7
justin <justinng@Justins-MacBook-Air.local>
2017-07-17 15:38:08 +0800

    implement clean cart function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,10 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+
+  def clean
+    current_cart.clean!
+    flash[:alert] = "Delete Selected Items"
+    redirect_to carts_path
+  end
+
 end
